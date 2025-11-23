9cf4a21b7ab5e15686049bdad8c22a07d89707ce
salmanvohra982-jpg <salmanvohra982@gmail.com>
2025-11-23 18:59:45 -0500

    Added placeholder code for reference

---
+++ client/src/MainRouter.jsx
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+import { Routes, Route } from "react-router-dom";
+import Projects from "./components/projects";
+import Home from "./components/Home";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
+import Layout from "./components/Layout";
+import Services from "./components/Services";
+import ListInventory from "./components/inventory/ListInventory";
+import AddInventory from "./components/inventory/AddInventory";
+import EditInventory from "./components/inventory/EditInventory";
+import Signin from "./components/auth/Signin";
+import Signup from "./components/auth/Signup";
+
+function MainRouter() {
+    return (
+        <div>
+            <Layout />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/users/signin" element={<Signin />} />
+                <Route path="/users/signup" element={<Signup />} />
+                <Route path="/inventory/list" element={<ListInventory />} />
+                <Route path="/inventory/add" element={<AddInventory />} />
+                <Route path="/inventory/edit/:id" element={<EditInventory />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default MainRouter;
---
+++ client/src/datasource/userModel.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+class UserModel
+{
+    constructor(firstName, lastName, email, password, username){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+}
+
+export default UserModel;
